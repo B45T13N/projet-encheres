b4e4d68c204b783a8eeef47976318c10d78dbc45
Dewey7796 <dsbastien7775@gmail.com>
2021-08-16 18:31:30 +0200

    Commit test connexion bdd

--- VentesAuxEncheres/src/org/Encheres/dal/ConnectionProvider.java
+++ VentesAuxEncheres/src/org/Encheres/dal/ConnectionProvider.java
@@ -12,16 +12,14 @@ abstract class ConnectionProvider {
 	private static DataSource dataSource;
 
 	static {
-		Context context;
-
 		try {
-			context = new InitialContext();
+			Context context = new InitialContext();
 			ConnectionProvider.dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx");
+
 		} catch (NamingException e) {
 			e.printStackTrace();
 			throw new RuntimeException("Impossible d'accéder à la base de données");
 		}
-
 	}
 
 	public static Connection getConnection() throws SQLException {
