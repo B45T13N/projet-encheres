40532d3429ff8e3fd254fa5b4e3a6385a6f8b522
christopheEni <christophe.collin2021@campus-eni.fr>
2021-08-17 10:53:23 +0200

    création bo enchere

--- VentesAuxEncheres/src/org/Encheres/bo/Enchere.java
+++ VentesAuxEncheres/src/org/Encheres/bo/Enchere.java
@@ -1,5 +1,41 @@
 package org.Encheres.bo;
 
-public class Enchere {
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Enchere implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private LocalDate dateEnchere;
+	private int montantEnchere;
+	
+	
+	
+	public LocalDate getDateEnchere() {
+		return dateEnchere;
+	}
+	public void setDateEnchere(LocalDate dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+	public int getMontantEnchere() {
+		return montantEnchere;
+	}
+	public void setMontantEnchere(int montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+	
+	public Enchere() {
+		
+	}
+	
+	public Enchere(LocalDate dateEnchere, int montantEnchere) {
+		super();
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+	}
+	
+	
+	
+	
 
 }
