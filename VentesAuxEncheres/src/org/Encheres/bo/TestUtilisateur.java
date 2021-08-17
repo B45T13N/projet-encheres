548c49dbc087a8ab0be2ee017336f8600df9efe6
Eklypses <pedro.martins84@hotmail.com>
2021-08-17 11:00:34 +0200

    Utilisateur + Test Utilisateur

---
+++ VentesAuxEncheres/src/org/Encheres/bo/TestUtilisateur.java
@@ -0,0 +1,13 @@
+package org.Encheres.bo;
+
+public class TestUtilisateur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Utilisateur perso = new Utilisateur(1,"Eklypse","Martins","Pedro","pedro@test.com",0610101010,"Rue 50",84500,"Bollène","Pedro8408",50,true);
+		System.out.println("Utilisateur :");
+		System.out.println(perso.toString());
+
+	}
+
+}
