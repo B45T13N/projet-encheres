c2094fb1ae441319ee1702601042ac909cb555a6
XavierNormandiere <xnormandiere@gmail.com>
2021-08-18 12:08:52 +0200

    Maj RetraitDAOJdbcImpl

--- VentesAuxEncheres/src/org/Encheres/dal/RetraitDAOJdbcImpl.java
+++ VentesAuxEncheres/src/org/Encheres/dal/RetraitDAOJdbcImpl.java
@@ -29,24 +29,25 @@ public class RetraitDAOJdbcImpl implements DAORetrait{
 		
 		try (Connection cnx = ConnectionProvider.getConnection())
 		{
+			
 			try {
 				cnx.setAutoCommit(false);
-				PreparedStatement prstms = cnx.prepareStatement(INSERT_RETRAITS, PreparedStatement.RETURN_GENERATED_KEYS);
+				PreparedStatement prstms = cnx.prepareStatement(INSERT_RETRAITS);
 				
 				if (data.getRue().length() < 30 || data.getCodePostal().length() < 15 || data.getVille().length() < 30) {
-					prstms.setString(1, data.getRue());
-					prstms.setString(2, data.getCodePostal());
-					prstms.setString(3, data.getVille());
+					prstms.setInt(1, data.getId());
+					prstms.setString(2, data.getRue());
+					prstms.setString(3, data.getCodePostal());
+					prstms.setString(4, data.getVille());
 				} else {
 					BusinessException busi = new BusinessException();
 					busi.ajouterErreur(10002);
 					throw busi;
 				}
-				ResultSet rs = prstms.getGeneratedKeys();
-				if(rs.next()) {
-					data.setId(rs.getInt(1));
-				}
-				rs.close();
+				
+				
+				
+				
 				prstms.close();
 				cnx.commit();
 				cnx.close();
