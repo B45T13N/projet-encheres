2be0792b379b4b12b849e9f83448612c70c6cf16
christopheEni <christophe.collin2021@campus-eni.fr>
2021-08-19 15:08:00 +0200

    commit jspcreationcompte(en cours), jsppageconnexion(terminé) commit servletcreationcompte(en cours), servletpageconnexion(terminé).

---
+++ VentesAuxEncheres/src/org/Encheres/Servlets/ServletCreationCompte.java
@@ -0,0 +1,39 @@
+package org.Encheres.Servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletCreationCompte
+ */
+@WebServlet("/ServletCreationCompte")
+public class ServletCreationCompte extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("connecte") != null) {
+			response.sendRedirect("/ServletAccueil");
+			
+		}else {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/CreationCompte.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ VentesAuxEncheres/src/org/Encheres/Servlets/ServletPageDeConnexion.java
@@ -0,0 +1,69 @@
+package org.Encheres.Servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.Encheres.bll.UtilisateurManager;
+import org.Encheres.bo.Utilisateur;
+
+
+/**
+ * Servlet implementation class ServletPageDeConnexion
+ */
+@WebServlet("/ServletPageDeConnexion")
+public class ServletPageDeConnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if(request.getParameter("connecte") != null) {
+			response.sendRedirect("/ServletAccueil");
+			
+		}else {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/PageDeConnexion.jsp");
+			rd.forward(request, response);
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		
+		String identifiant;
+		String mdp;
+		
+		try
+		{
+			identifiant = request.getParameter("identifiant");
+			mdp = request.getParameter("mdp");
+			UtilisateurManager utilisateurManager = new UtilisateurManager();
+			Utilisateur utilisateur = utilisateurManager.getUtilisateur(identifiant, mdp);
+			request.setAttribute("connecte", utilisateur);
+			
+		}catch(Exception e ) {
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/Accueil);
+		rd.forward(request, response);
+	}
+
+		}
+
+
+
