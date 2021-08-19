aba92a1bb1abb77a9aa02c1daca672a295e52f6b
XavierNormandiere <xnormandiere@gmail.com>
2021-08-19 09:17:17 +0200

    suite

--- VentesAuxEncheres/src/org/Encheres/Servlets/ServletAccueilDeconnecte.java
+++ VentesAuxEncheres/src/org/Encheres/Servlets/ServletAccueilDeconnecte.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletAccueilDeconnecte extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	
+	// pas super
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
