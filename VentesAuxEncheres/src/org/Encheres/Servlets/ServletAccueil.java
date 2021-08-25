4c9e0c7a15f7e93fa6f7d33cb81bdc566a315e58
Eklypses <pedro.martins84@hotmail.com>
2021-08-25 12:35:46 +0200

    Commit Servlet Accueil

--- VentesAuxEncheres/src/org/Encheres/Servlets/ServletAccueil.java
+++ VentesAuxEncheres/src/org/Encheres/Servlets/ServletAccueil.java
@@ -1,9 +1,6 @@
 package org.Encheres.Servlets;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +16,6 @@ import org.Encheres.BusinessException;
 import org.Encheres.bll.ArticleManager;
 import org.Encheres.bll.UtilisateurManager;
 import org.Encheres.bo.Article;
-import org.Encheres.dal.CodesResultatDAL;
-import org.Encheres.dal.JDBCTools.ConnectionProvider;
 
 @WebServlet("/Accueil")
 public class ServletAccueil<BeanFiltreRecherche> extends HttpServlet {
@@ -71,14 +66,6 @@ public class ServletAccueil<BeanFiltreRecherche> extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 
-		final String SELECT_ALL_ARTICLES = "SELECT * FROM ARTICLES_VENDUS WHERE date_fin_encheres > GETDATE() ORDER BY date_fin_encheres DESC";
-		final String SELECT_ENCHERES_USER = "SELECT * FROM ARTICLES_VENDUS WHERE no_utilisateur =18 AND date_fin_encheres > GETDATE() ORDER BY date_fin_encheres DESC";
-		final String SELECT_ENCHERES_USER_CLAIMED = "SELECT * FROM ARTICLES_VENDUS WHERE no_utilisateur =18 AND date_fin_encheres < GETDATE() ORDER BY date_fin_encheres DESC";
-		final String SELECT_SALES_IN_PROGRESS = "SELECT * FROM ARTICLES_VENDUS WHERE no_utilisateur =? AND date_fin_encheres > GETDATE() ORDER BY date_fin_encheres DESC";
-		final String SELECT_SALES_NOT_STARTED = "SELECT * FROM ARTICLES_VENDUS WHERE no_utilisateur =? AND date_debut_encheres < GETDATE() ORDER BY date_debut_encheres DESC";
-		final String SELECT_SALES_SOLD = "SELECT * FROM ARTICLES_VENDUS WHERE no_utilisateur =? AND date_fin_encheres < GETDATE() ORDER BY date_fin_encheres DESC";
-		List<Article> list = new ArrayList<Article>();
-
 		String categorie;
 		ArticleManager am = new ArticleManager();
 		UtilisateurManager um = new UtilisateurManager();
@@ -109,193 +96,24 @@ public class ServletAccueil<BeanFiltreRecherche> extends HttpServlet {
 
 				if (request.getParameter("enCours") != null) {
 
-					try (Connection cnx = ConnectionProvider.getConnection()) {
-						try {
-							cnx.setAutoCommit(false);
-							// Mise à jour article
-							PreparedStatement prstms = cnx.prepareStatement(SELECT_ALL_ARTICLES);
-							ResultSet rs = prstms.executeQuery();
-
-							Article art = null;
-							while (rs.next()) {
-								art = new Article(rs.getInt("no_utilisateur"), rs.getString("nom_article"),
-										rs.getString("description"), rs.getString("libelle"),
-										rs.getDate("date_fin_encheres").toLocalDate(), rs.getInt("prix_vente"),
-										rs.getString("pseudo"));
-								art.setNoArticle(rs.getInt("noArticle"));
-								list.add(art);
-							}
-							prstms.close();
-							cnx.close();
-
-						} catch (Exception e) {
-							e.printStackTrace();
-
-						}
-					} catch (Exception e) {
-						BusinessException businessException = new BusinessException();
-						businessException.ajouterErreur(CodesResultatDAL.LECTURE_ARTICLE_FAIL);
-						throw businessException;
-					}
 				}
 				if (request.getParameter("mesEnCours") != null) {
-					try (Connection cnx = ConnectionProvider.getConnection()) {
-						try {
-							cnx.setAutoCommit(false);
-							// Mise à jour article
-							PreparedStatement prstms = cnx.prepareStatement(SELECT_ENCHERES_USER);
-							ResultSet rs = prstms.executeQuery();
-
-							Article art = null;
-							while (rs.next()) {
-								art = new Article(rs.getInt("no_utilisateur"), rs.getString("nom_article"),
-										rs.getString("description"), rs.getString("libelle"),
-										rs.getDate("date_fin_encheres").toLocalDate(), rs.getInt("prix_vente"),
-										rs.getString("pseudo"));
-								art.setNoArticle(rs.getInt("noArticle"));
-								list.add(art);
-							}
-							prstms.close();
-							cnx.close();
-
-						} catch (Exception e) {
-							e.printStackTrace();
-
-						}
-					} catch (Exception e) {
-						BusinessException businessException = new BusinessException();
-						businessException.ajouterErreur(CodesResultatDAL.LECTURE_ARTICLE_FAIL);
-						throw businessException;
-					}
 
 				}
 				if (request.getParameter("remportes") != null) {
-					try (Connection cnx = ConnectionProvider.getConnection()) {
-						try {
-							cnx.setAutoCommit(false);
-							// Mise à jour article
-							PreparedStatement prstms = cnx.prepareStatement(SELECT_ENCHERES_USER_CLAIMED);
-							ResultSet rs = prstms.executeQuery();
 
-							Article art = null;
-							while (rs.next()) {
-								art = new Article(rs.getInt("no_utilisateur"), rs.getString("nom_article"),
-										rs.getString("description"), rs.getString("libelle"),
-										rs.getDate("date_fin_encheres").toLocalDate(), rs.getInt("prix_vente"),
-										rs.getString("pseudo"));
-								art.setNoArticle(rs.getInt("noArticle"));
-								list.add(art);
-							}
-							prstms.close();
-							cnx.close();
-
-						} catch (Exception e) {
-							e.printStackTrace();
-
-						}
-					} catch (Exception e) {
-						BusinessException businessException = new BusinessException();
-						businessException.ajouterErreur(CodesResultatDAL.LECTURE_ARTICLE_FAIL);
-						throw businessException;
-					}
 				}
 			}
 			// check box filtreVente
-			if (request.getParameter("radioBtn") != null && request.getParameter("radioBtn").equals("2"))
-
-			{
+			if (request.getParameter("radioBtn") != null && request.getParameter("radioBtn").equals("2")) {
 
 				if (request.getParameter("venteEnCours") != null) {
-					try (Connection cnx = ConnectionProvider.getConnection()) {
-						try {
-							cnx.setAutoCommit(false);
-							// Mise à jour article
-							PreparedStatement prstms = cnx.prepareStatement(SELECT_SALES_IN_PROGRESS);
-							ResultSet rs = prstms.executeQuery();
-
-							Article art = null;
-							while (rs.next()) {
-								art = new Article(rs.getInt("no_utilisateur"), rs.getString("nom_article"),
-										rs.getString("description"), rs.getString("libelle"),
-										rs.getDate("date_fin_encheres").toLocalDate(), rs.getInt("prix_vente"),
-										rs.getString("pseudo"));
-								art.setNoArticle(rs.getInt("noArticle"));
-								list.add(art);
-							}
-							prstms.close();
-							cnx.close();
-
-						} catch (Exception e) {
-							e.printStackTrace();
-
-						}
-					} catch (Exception e) {
-						BusinessException businessException = new BusinessException();
-						businessException.ajouterErreur(CodesResultatDAL.LECTURE_ARTICLE_FAIL);
-						throw businessException;
-					}
 
 				}
 				if (request.getParameter("venteNonDebut") != null) {
-					try (Connection cnx = ConnectionProvider.getConnection()) {
-						try {
-							cnx.setAutoCommit(false);
-							// Mise à jour article
-							PreparedStatement prstms = cnx.prepareStatement(SELECT_SALES_NOT_STARTED);
-							ResultSet rs = prstms.executeQuery();
-
-							Article art = null;
-							while (rs.next()) {
-								art = new Article(rs.getInt("no_utilisateur"), rs.getString("nom_article"),
-										rs.getString("description"), rs.getString("libelle"),
-										rs.getDate("date_fin_encheres").toLocalDate(), rs.getInt("prix_vente"),
-										rs.getString("pseudo"));
-								art.setNoArticle(rs.getInt("noArticle"));
-								list.add(art);
-							}
-							prstms.close();
-							cnx.close();
-
-						} catch (Exception e) {
-							e.printStackTrace();
-
-						}
-					} catch (Exception e) {
-						BusinessException businessException = new BusinessException();
-						businessException.ajouterErreur(CodesResultatDAL.LECTURE_ARTICLE_FAIL);
-						throw businessException;
-					}
 
 				}
 				if (request.getParameter("venteTerminee") != null) {
-					try (Connection cnx = ConnectionProvider.getConnection()) {
-						try {
-							cnx.setAutoCommit(false);
-							// Mise à jour article
-							PreparedStatement prstms = cnx.prepareStatement(SELECT_SALES_SOLD);
-							ResultSet rs = prstms.executeQuery();
-
-							Article art = null;
-							while (rs.next()) {
-								art = new Article(rs.getInt("no_utilisateur"), rs.getString("nom_article"),
-										rs.getString("description"), rs.getString("libelle"),
-										rs.getDate("date_fin_encheres").toLocalDate(), rs.getInt("prix_vente"),
-										rs.getString("pseudo"));
-								art.setNoArticle(rs.getInt("noArticle"));
-								list.add(art);
-							}
-							prstms.close();
-							cnx.close();
-
-						} catch (Exception e) {
-							e.printStackTrace();
-
-						}
-					} catch (Exception e) {
-						BusinessException businessException = new BusinessException();
-						businessException.ajouterErreur(CodesResultatDAL.LECTURE_ARTICLE_FAIL);
-						throw businessException;
-					}
 
 				}
 			}
